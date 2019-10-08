5cf7d72ade0f6c9b7b2f3b12d38445fc503e0e53
Andrey Zhaldybin <33570153+Aneryd@users.noreply.github.com>
2019-10-08 19:03:15 +0300

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,32 @@
+﻿#include <math.h>
+#include <stdio.h>     
+
+int main()
+{
+	float a, x, s, f, g, y;
+	int funct;
+	double pi = 3.141592653589;
+	printf("Введите x: ");
+	scanf_s("%f", &x);
+	printf("Введите a: ");
+	scanf_s("%f", &a);
+	printf("Выберите какую ф-ию посчитать: f - 1, g -2, y - 3 ");
+	scanf_s("%i", &funct);
+
+	if (funct == 1) {
+		f = (sin(pi * (10 * a * a + 37 * a * x + 7 * x * x)));
+		printf("%f", f);
+	}
+	else if (funct == 2) {
+		g = -1 * (2 * (-5 * a * a + 3 * a * x + 2 * x * x) / (5 * a * a + 9 * a * x - 2 * x * x));
+		printf("%f", g);
+	}
+	else if (funct == 3) {
+		y = (log(-5 * a * a - 16 * a * x + 16 * x * x + 1) / log(2));
+		printf("%f", y);
+	}
+	else {
+		printf("ERROR");
+	}
+}
+
