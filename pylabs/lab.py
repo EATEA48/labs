c62ee85074e031dfa5de8edbd383147bc324305d
Aneryd <andrey.zhalo@mail.ru>
2019-11-18 19:57:01 +0300

    labs

--- pylabs/lab.py
+++ pylabs/lab.py
@@ -15,14 +15,18 @@ mf_max = 0
 my_max = 0
 
 while p >= 0:
+    # Проверка на кол-во счетчика
     if p == 0:
         question = input("Вы хотите продолжить?(да, нет)")
+        # Проверка на продолжение программы
         if question == 'да':
             p = int(input('Сколько счтитать?'))
             sh = float(input('С каким шагом?: '))
         else:
             print('END')
+    # Проверка на ограничения
     if a != 0 and a != 1:
+        # Ф-ия g
         if fun == 'g':
             for i in range(p):
                 g = (-1 * (2 * (-5 * a ** 2 + 3 * a * x + 2 * x ** 2) / (5 * a ** 2 + 9 * a * x - 2 * x ** 2)))
@@ -33,7 +37,7 @@ while p >= 0:
                         mg_max = g
                     mg.append(g)
                     a += sh
-
+        # Ф-ия f
         elif fun == 'f':
             for i in range(p):
                 f = (sin(pi * (10 * a ** 2 + 37 * a * x + 7 * x ** 2)))
@@ -44,7 +48,7 @@ while p >= 0:
                         mf_max = f
                         mf.append(f)
                     a += sh
-
+        # Ф-ия y
         elif fun == 'y':
             for i in range(p):
                 y = (log(-5 * a ** 2 - 16 * a * x + 16 * x ** 2 + 1) / log(2))
@@ -58,12 +62,19 @@ while p >= 0:
         else:
             print("ERROR")
         p -= 1
+    # Проверка на окончание цикла
+    if p == 0:
+        # Вывод выбранной ф-ии и ее max э-та
+        if fun == 'g':
+            print('Максимальный эл-т массива mg: ' + str(mg_max))
+            print('{ ' + str(mg) + '\n')
+            print('}')
+        if fun == 'f':
+            print('Максимальный эл-т массива mf: ' + str(mf_max))
+            print(mf)
+        if fun == 'y':
+            print('Максимальный эл-т массива my: ' + str(my_max))
+            print(my)
 
-    print('Максимальный эл-т массива mg: ' + str(mg_max))
-    print('Максимальный эл-т массива mf: ' + str(mf_max))
-    print('Максимальный эл-т массива my: ' + str(my_max))
-    print()
-    print(mg)
-    print(mf)
-    print(my)
+
 
