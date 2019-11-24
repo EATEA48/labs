1064bd567b2eb473ca311b0de5d56b00df9f160a
Aneryd <andrey.zhalo@mail.ru>
2019-11-24 22:40:24 +0300

    labs

--- semworks/image processing/3/3.py
+++ semworks/image processing/3/3.py
@@ -1,6 +1,13 @@
+
 import cv2
+import numpy as np
 
 img = cv2.imread('Lenna.png')
 blur = cv2.GaussianBlur(img, (51, 51), 0)
-cv2.imshow(' ', blur)
+# cv2.imshow(' ', blur)
+# cv2.waitKey(0)
+
+# Объеденяет и выводит сразу 2 изображения
+vis = np.concatenate((img, blur), axis=1)
+cv2.imshow('', vis)
 cv2.waitKey(0)
