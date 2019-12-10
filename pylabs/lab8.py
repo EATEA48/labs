20eae019a56aad1bb087bfa6d68ceef7e7534b32
Aneryd <andrey.zhalo@mail.ru>
2019-12-10 22:05:56 +0300

    labs

--- pylabs/lab8.py
+++ pylabs/lab8.py
@@ -1,15 +1,49 @@
+import time
 import random
-import defs_lab8
+import math
+begin = time.process_time()
+def coordinates(kol):
+    data = []
+    for i in range(kol):
+        x = random.uniform(0, 500)
+        y = random.uniform(0, 500)
+        data.append((x,y))
+    return(data)
 
-x = []
-y = []
+def centre(dots):
+    c = random.choice(dots)
+    return(c)
 
-# N = int(input("Кол-во точек: "))
+def kollvo(tochki, centr, radius):
+    count = 0
+    for i in tochki:
+        a = i
+        if math.pow(a[0] - centr[0], 2) - math.pow(a[1] - centr[1], 2) <= math.pow(radius, 2):
+            count+=1
+    return(count)
 
-print(defs_lab8.random_x_y(5))
+while True:
+    try:
+        r = float(input('Введи радиус: '))
+        point = int(input('Введи кол-во точек: '))
+        break
+    except:
+        print('Попробуй еще разок')
+points = coordinates(point)
+print('Список точек: ', points)
+mid = centre(points)
+print()
+print('Центр окружности: ', mid)
+print()
+ans = kollvo(points, mid, r)
+if ans == 0:
+    print('В данную область точки не входят')
+else:
+    print('Точек, входящих в окружность: ', ans)
 
-radius = float(input("введите радиус: "))
-
-
+time = (time.process_time() - begin)
+file = open('time.txt', 'a')
+file.write(str(time) + '\n')
+file.close()
 
 
