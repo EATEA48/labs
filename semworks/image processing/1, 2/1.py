36ece56a9733dd0df474d98f3ca41bc4c284855d
dd <a034871vz@gmail.com>
2019-12-18 10:57:29 +0300

    labs

--- semworks/image processing/1, 2/1.py
+++ semworks/image processing/1, 2/1.py
@@ -1,34 +1,21 @@
-# import cv2
-#
-# img = cv2.imread('Lenna.png')
-# cv2.imshow('Original image', img)
-# cv2.waitKey(0)
-#
-# grayimg = cv2.cvtColor(img[..., 0], cv2.COLOR_GRAY2RGB)
-# cv2.imshow('Gray image', grayimg)
-# cv2.waitKey(0)
-
-# cv2.imwrite('gray.png', grayimg)
-
-import random
 from PIL import Image, ImageDraw
 
-# mode = 0
-# image = Image.open('Lenna.png')
-# draw = ImageDraw.Draw(image)
-# width = image.size[0]
-# height = image.size[1]
-# pix = image.load()
-#
-# if (mode == 0):
-#     for i in range(width):
-#         for j in range(height):
-#             a = pix[i, j][0]
-#             b = pix[i, j][1]
-#             c = pix[i, j][2]
-#             s = (a + b + c) // 3
-#             draw.point((i, j), (s, s, s))
+img = Image.open("Lenna.png")
 
+draw = ImageDraw.Draw(img)
 
+w = img.size[0]
+h = img.size[1]
 
+pix = img.load()
 
+for i in range(w):
+    for j in range(h):
+        a = pix[i, j][0]
+        b = pix[i, j][1]
+        c = pix[i, j][2]
+        s = (a + b + c) // 3
+        draw.point((i, j), (s, s, s))
+img.show()
+img = img.save("gray.png")
+img.close()
--- semworks/image processing/3/3.py
+++ semworks/image processing/3/3.py
@@ -1,13 +1,28 @@
+from PIL import Image, ImageDraw
 
-import cv2
-import numpy as np
+img = Image.open("Lenna.png")
 
-img = cv2.imread('Lenna.png')
-blur = cv2.GaussianBlur(img, (51, 51), 0)
-# cv2.imshow(' ', blur)
-# cv2.waitKey(0)
+draw = ImageDraw.Draw(img)
 
-# Объеденяет и выводит сразу 2 изображения
-vis = np.concatenate((img, blur), axis=1)
-cv2.imshow('', vis)
-cv2.waitKey(0)
+w = img.size[0]
+h = img.size[1]
+
+matrix = [[0.5, 0.75, 0.5],
+          [0.75, 1, 0.75],
+          [0.5, 0.75, 0.5]
+          ]
+
+div = 1 / 6
+
+pix = img.load()
+for z in range(len(matrix)):
+    m = matrix[z]
+    for i in range(w):
+        for j in range(h):
+            a = pix[i, j][0]
+            b = pix[i, j][1]
+            c = pix[i, j][2]
+            S = pix[i, j] * matrix[z, i]
+            draw.point((i, j), (int(a), int(b), int(c)))
+img.show()
+img.close()
