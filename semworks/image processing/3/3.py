135790fda08144eb6c3eae9ba4e6a41d8cb3eddc
ALexey Morozow <ugly.duckling357@gmail.com>
2019-12-18 15:58:59 +0300

    labs

--- semworks/image processing/3/3.py
+++ semworks/image processing/3/3.py
@@ -1,28 +1,49 @@
 from PIL import Image, ImageDraw, ImageFilter
 import math
-image = Image.open('Lenna.png')
-draw = ImageDraw.Draw(image)
-w = image.size[0]
-h = image.size[1]
-pix = image.load()
+# image = Image.open('Lenna.png')
+# draw = ImageDraw.Draw(image)
+# w = image.size[0]
+# h = image.size[1]
+# pix = image.load()
+#
+#
+# sigma = 1
+# k = 0
+#
+# r = 10
 
-k = 0
+img = Image.open('Lenna.png').convert('L')
+r = 3
 
-r = 10
+draw = ImageDraw.Draw(img)
 
-matrix = [
-    [0, 1, 0],
-    [1, 0, 1],
-    [0, 1, 0]
-]
+def gauss_blur(img, r):
+    imgData = list(img.getdata())
 
-for i in range(len(matrix)):
-    for j in range(len(matrix[i-1])):
-            if (matrix[i + 1] or matrix[i - 1]) and (matrix[j + 1] or matrix[j - 1]) and (j != 2) and (i != 2):
-                print(matrix[i][j])
-            else:
-                i += 1
+    bluredImg = Image.new(img.mode, img.size)
+    bluredImgData = list(bluredImg.getdata())
 
+    rs = int(math.ceil(r * 2.57))
+
+    for i in range(0, img.height):
+        for j in range(0, img.width):
+            val = 0
+            wsum = 0
+            for iy in range(i - rs, i + rs + 1):
+                for ix in range(j - rs, j + rs + 1):
+                    x = min(img.width - 1, max(0, ix))
+                    y = min(img.height - 1, max(0, iy))
+                    dsq = (ix - j) * (ix - j) + (iy - i) * (iy - i)
+                    weight = math.exp(-dsq / (2 * r * r)) / (math.pi * 2 * r * r)
+                    val += imgData[y * img.width + x] * weight
+                    wsum += weight
+            bluredImgData[i * img.width + j] = round(val / wsum)
+
+    # bluredImg.putdata(bluredImgData)
+    # return bluredImg
+    draw.point(bluredImgData)
+    img.show()
+img.close()
 
 
 # for i in range(w):
