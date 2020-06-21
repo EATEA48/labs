99b9d788951a0d09598669e2c09b5d937d42f07f
Andrey Zhaldybin <33570153+Aneryd@users.noreply.github.com>
2020-06-21 11:55:52 +0300

    Лабы

--- sem2/SystemLinUr/Kramer/main.c
+++ sem2/SystemLinUr/Kramer/main.c
@@ -1,62 +1,87 @@
 #include <math.h>
 #include <stdio.h>
-#include <time.h>
-
-int Kramer(float a11,float a12,
-           float a21,float a22,
-           float b1 ,float b2,
-           float *x ,float *y)
+#include <stdlib.h>
+#include <omp.h>
+struct _result
 {
-    float det,detx,dety;
-    det =a11*a22-a12*a21;
-    detx=b1*a22-b2*a12;
-    dety=a11*b2-b1*a21;
+    int _res;
+    int X;
+    int Y;
+};
+
+typedef struct _result result;
+
+
+result Kramer(int *A, int *B, float *X,float *Y)
+{
+    float det, detx, dety;
+    float A11,A12,B1,A21,A22,B2;
+    result res;
+    A11 = rand();
+    A12 = rand();
+    B1 = rand();
+    A21 = rand();
+    A22 = rand();
+    B2 = rand();
+    X = rand();
+    Y = rand();
+
+    det =A11*A22-A12*A21;
+    detx=A11*A22-B2*A12;
+    dety=A11*B2-B1*A21;
 
     if (fabs(det) > 1.0E-14)
     {
-        *x=detx/det;
-        *y=dety/det;
-        return 0; /* единственное решение */
+        res.X = detx/det;
+        res.Y = dety/det;
+        res._res = 0;
+        return res; /* единственное решение */
     }
     else
-    if ((fabs(detx) <1.0E-14) && (fabs(dety) <1.0E-14))
-        return 1; /* бесконечное множество решений */
+    if ((fabs(detx) <1.0E-14) && (fabs(dety) <1.0E-14)) {
+        res._res = 1;
+        return res; /* бесконечное множество решений */
+    }
     else
-        return -1; /* решений нет */
+    {
+        res._res = -1;
+        return res; /* решений нет */
+    }
 }
+//    else
+//    if ((fabs(detx) <1.0E-14) && (fabs(dety) <1.0E-14))
+//        return 1; /* бесконечное множество решений */
+//    else
+//        return -1; /* решений нет */
+
 
 int main(int argc, char* argv[])
 {
     float A11,A12,B1,A21,A22,B2,X,Y;
-    int rc;
+    int res;
+    double start_time = omp_get_wtime();
+    for (int array_size_mult = 10000000; array_size_mult < 110000000; array_size_mult=array_size_mult+10000000) {
+        int *A = (int *) malloc(sizeof(int) * array_size_mult);
+        int *B = (int *) malloc(sizeof(int) * array_size_mult);
+        res=(A11,A12,A21,A22,B1,B2,&X,&Y);
+        double stop_time = omp_get_wtime();
+        printf("%lf\n",stop_time - start_time);
+        free(A);
+        free(B);
 
-    printf("A11=");
-    scanf("%e",&A11);
-    printf("A12=");
-    scanf("%e",&A12);
-    printf("B1=");
-    scanf("%e",&B1);
+    }
 
-    printf("A21=");
-    scanf("%e",&A21);
-    printf("A22=");
-    scanf("%e",&A22);
-    printf("B2=");
-    scanf("%e",&B2);
+//    rc=(A11,A12,A21,A22,B1,B2,&X,&Y);
+//
+//    if (rc == 0)
+//        printf("X=%e\nY=%e\n",X,Y);
+//    else
+//    if (rc == 1)
+//        printf("Infinite set of roots...\n");
+//    else
+//        printf("Set of roots is empty...\n");
 
-    rc=Kramer(A11,A12,A21,A22,B1,B2,&X,&Y);
 
-    if (rc == 0)
-        printf("X=%e\nY=%e\n",X,Y);
-    else
-    if (rc == 1)
-        printf("Infinite set of roots...\n");
-    else
-        printf("Set of roots is empty...\n");
-
-//    time_t t0 = time(0);
-//    time_t t1 = time(1);
-//    double time_in_sec = difftime(t1, t0);
 
     return 0;
 }
